a27e8559a18251e1b3f287a12bb85cd03968e17b
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-02-16 21:34:34 +0600

    Randor Alphabet Generate Done

--- script.js
+++ script.js
@@ -11,8 +11,35 @@ function showSection(elementId) {
 
 }
 
+function generateAlphabet(){
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetSplit = alphabets.split('');
+    const randomNumber = Math.random()*25;
+    const randomAlphabet = Math.round(randomNumber);
+
+    const alphabet = alphabets[randomAlphabet];
+    return alphabet;
+
+}
+
+function setKeyboardColor(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400');
+}
+
+
+
+function continueGame(){
+    const alphabet = generateAlphabet();
+    const currentAlphabetElement = document.getElementById('currentAlphabet');
+    currentAlphabetElement.innerHTML = alphabet.toUpperCase();
+    setKeyboardColor(alphabet);
+    
+
+}
 function playNow() {
     hideSection('dashboard');
     showSection('gameControl');
-    // console.log('I am Working');
+    continueGame();
+    
 }
