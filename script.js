cd214665abcca320bfdca861ff3cdffa5f87b04f
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-02-17 13:09:59 +0600

    Life and Score function Working

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-function hideSection(elementId){
+function hideSection(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
-    
+
 }
 
 function showSection(elementId) {
@@ -11,35 +11,81 @@ function showSection(elementId) {
 
 }
 
-function generateAlphabet(){
+function getTextElementValue(elementId){
+    const textElementId = document.getElementById(elementId);
+    const textElement = textElementId.innerText;
+    const value = parseInt(textElement);
+    return value;
+
+}
+function setTextElementValue(elementId,value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+  
+
+}
+
+function generateAlphabet() {
     const alphabets = 'abcdefghijklmnopqrstuvwxyz';
     const alphabetSplit = alphabets.split('');
-    const randomNumber = Math.random()*25;
+    const randomNumber = Math.random() * 25;
     const randomAlphabet = Math.round(randomNumber);
-
     const alphabet = alphabets[randomAlphabet];
     return alphabet;
 
 }
 
-function setKeyboardColor(elementId){
+function setKeyboardColor(elementId) {
     const element = document.getElementById(elementId)
     element.classList.add('bg-orange-400');
 }
+function removeKeyboardColor(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400');
+}
+
+function handleKeyboardPress(event) {
+    
+    const playerPress = event.key;
+    const currentAlphabetElement = document.getElementById('currentAlphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    if (playerPress === expectedAlphabet) {
+        const currentScore = getTextElementValue('scoreCount');
+        const updateScore = currentScore + 1;
+        setTextElementValue('scoreCount',updateScore);
+
+        removeKeyboardColor(expectedAlphabet);
+        continueGame();
+
+    }
+    else{
+        const currentScore = getTextElementValue('lifeCount');
+        const updateScore = currentScore - 1;
+        setTextElementValue('lifeCount', updateScore);
+
+        removeKeyboardColor(expectedAlphabet);
+        continueGame();
+
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardPress);
 
 
 
-function continueGame(){
+function continueGame() {
     const alphabet = generateAlphabet();
     const currentAlphabetElement = document.getElementById('currentAlphabet');
     currentAlphabetElement.innerHTML = alphabet.toUpperCase();
     setKeyboardColor(alphabet);
-    
+
 
 }
 function playNow() {
     hideSection('dashboard');
     showSection('gameControl');
     continueGame();
-    
+
 }
